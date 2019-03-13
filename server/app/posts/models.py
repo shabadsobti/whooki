cc6011ae0bf5ed5001fb92bcd79a5842a75ef637
Shabad Sobti <shabad.sobti@gmail.com>
2019-03-13 15:20:29 -0400

    Posts model added

--- server/app/posts/models.py
+++ server/app/posts/models.py
@@ -3,10 +3,12 @@ from datetime import datetime
 from django.utils import timezone
 
 
-# class Post(models.Model):
-#     username = models.CharField(max_length=20)
-#     website = models.CharField(max_length=100)
-#     votes = models.IntegerField()
-#
-#     def __str__(self):
-#         return self.username
+class Post(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    text = models.CharField(max_length=120)
+    website = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
